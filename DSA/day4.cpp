fe7046691cfe373ac9512096f2d1d602cc4227b7
Rithish-Krishna <145421417+Rithish-Krishna@users.noreply.github.com>
2026-06-13 18:51:15 +0530

    added the prcedure

--- DSA/day4.cpp
+++ DSA/day4.cpp
@@ -1,3 +1,22 @@
+//patern solved
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+		
+// LEARNED 
+// THE FIRST LOOP USED TO CONTROL NO.OF LINES
+// THE SECOND LOOP IS FOR THE COLUMN CONTROL
+		
+// USED VARIABLE LEFT AND RIGHT TO CONTROL THE STARS AT LEFT AND RIIGHT 
+// STARTS WITH LEFT = N RIGHT = N
+// THE STAR IS PRINTED IF IT'S POSITION IS GREATER THAN OR EQUAL TO LEFT AND LESS THAN OR EQUAL TO RIGHT 
+		
+// 12345(*)6789      LEFT = 5 RIGHT = 5
+// 1234(*)5(*)6(*)789  LEFT = 4 RIGHT = 6
+// 123(*)4(*)5(*)6(*)7(*)89	LEFT = 3 RIGHT = 7	
+		
 #include<iostream>
 using namespace std;
 int main(){
