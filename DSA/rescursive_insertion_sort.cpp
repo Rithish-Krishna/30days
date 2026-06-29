caa8ae756ce03e44bc25272ca3b9ebde84befa6c
Rithish-Krishna <rithishkrishna24@gmail.com>
2026-06-29 19:49:20 +0530

    Started pandas -> series, df

--- DSA/rescursive_insertion_sort.cpp
+++ DSA/rescursive_insertion_sort.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include <bits/stdc++.h>
 using namespace std;
-void is(arr,i){
+void is(int arr[],int i,int n){
 	if(i==n) return;
 	int temp = arr[i];
 	int j=i-1;
@@ -10,10 +10,15 @@ void is(arr,i){
 		j--;
 	}
 	arr[j+1] = temp;
-	is(arr,i+1);
+	is(arr,i+1,n);
 	
 }
 
 int main(){
+	int arr[] = {5,3,52,3};
+	int n=4;
+	is(arr,0,n);
 	
+	for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
 }
